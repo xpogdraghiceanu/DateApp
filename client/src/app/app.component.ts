40a56af447660fa74e630c2bd2cf95e19ba94c51
xpogdraghiceanu <george.draghiceanu@xpo.com>
2020-10-13 12:43:12 +0300

    Angular Skelethon end section

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Dating App';
+  users: any;
+ 
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.getUsers();
+  }
+
+  getUsers () {
+    this.http.get("https://localhost:5001/api/Users").subscribe(response => {
+      this.users = response;
+    });
+  }
 }
+
+
