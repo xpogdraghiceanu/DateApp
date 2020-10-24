9208707bce9aa79d2b6828b93daacddcc0a44586
Catalina Elena NITA <canita@bbu.bitdefender.biz>
2020-10-24 09:05:56 +0300

    End of section5: Client Login

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { AccountService } from './_services/account.service';
 
 @Component({
   selector: 'app-root',
@@ -10,17 +11,23 @@ export class AppComponent implements OnInit {
   title = 'Dating App';
   users: any;
  
-  constructor(private http: HttpClient) {}
+  constructor(private accountService: AccountService) {}
 
   ngOnInit(): void {
-    this.getUsers();
+    // this.getUsers();
+    this.setCurrentuser();
   }
 
-  getUsers () {
-    this.http.get("https://localhost:5001/api/Users").subscribe(response => {
-      this.users = response;
-    });
+  setCurrentuser() {
+    const user = JSON.parse(localStorage.getItem('user'));
+    this.accountService.setCurrentUser(user);
   }
+
+  // getUsers () {
+  //   this.http.get("https://localhost:5001/api/Users").subscribe(response => {
+  //     this.users = response;
+  //   });
+  // }
 }
 
 
--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AccountService } from '../_services/account.service';
 
 @Component({
   selector: 'app-nav',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavComponent implements OnInit {
 
-  constructor() { }
+  model: any = {};
+
+  constructor(public accountService: AccountService) { }
 
   ngOnInit(): void {
   }
+
+  login() {
+    this.accountService.login(this.model).subscribe(response => {
+      console.log(response);
+    }, error => console.log(error));
+  }
+
+  logout() {
+    this.accountService.logout();
+  }
+
+
 
 }
